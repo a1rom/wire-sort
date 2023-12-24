fc062807bc20432205cd5f81037c494f7e1e663c
a1rom <dev@alrom.de>
2023-12-24 17:01:05 +0200

    create table components

---
+++ resources/views/components/table/td-cell.blade.php
@@ -0,0 +1,37 @@
+@props([
+    'scope' => 'col',
+    'firstInRow' => false,
+    'hidden' => false,
+    'functional' => false,
+    'value' => '',
+    'nowrap' => false,
+])
+@php
+$styleClass = 'px-3 py-4 text-sm';
+    $firstInRow ? $styleClass .= ' font-medium text-gray-900 whitespace-nowrap' : $styleClass .= ' font-normal text-gray-700';
+
+    if ($hidden) {
+        $styleClass .= ' hidden';
+        if ($hidden == 'sm') {
+            $styleClass .= ' sm:table-cell';
+        }
+        if ($hidden == 'md') {
+            $styleClass .= ' md:table-cell';
+        }
+    }
+
+    if ($nowrap) {
+        $styleClass .= ' whitespace-nowrap';
+    }
+
+    if ($functional) {
+        $styleClass = 'py-4 pl-3 text-right text-sm font-medium';
+    }
+
+@endphp
+
+<th scope="{{$scope}}"
+    class="{{ $styleClass }}"
+    >
+    {{ $value }}
+</th>
