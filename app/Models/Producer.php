ac45045652c6542369d8d61cb2a4a7246eafc1da
a1rom <dev@alrom.de>
2023-12-24 17:50:18 +0200

    Add products relationship to Producer model

--- app/Models/Producer.php
+++ app/Models/Producer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Producer extends Model
 {
@@ -14,4 +15,12 @@ class Producer extends Model
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * @return HasMany<Product>
+     */
+    public function products() : HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
